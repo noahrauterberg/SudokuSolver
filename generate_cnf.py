c1c0696decce5eccc3be93083e57131e9cf1a482
noah <rauterberg@campus.tu-berlin.de>
2024-01-25 18:53:29 +0100

    exporting cnf formular to DIMACS cnf format and parsing

--- generate_cnf.py
+++ generate_cnf.py
@@ -1,5 +1,30 @@
 from z3 import *
 
+
+def write_cnf(file, clause, end):
+    if is_not(clause):
+        file.write(f"-{clause.children()[0]}{end}")
+    elif is_or(clause):
+        for child in clause.children():
+            write_cnf(file, child, " ")
+        file.write(" 0\n")
+    else:
+        file.write(f"{clause}{end}")
+
+
+def cnf_to_file(filename):
+    """Generates a file almost in the DIMACS cnf format, but variables have complete names instead of just numbers
+
+    Args:
+        filename (string): name of the file where the cnf formular is being written
+    """
+    with open(filename, "w") as f:
+        # the first line of the file gives some information on the formular
+        f.write(f"p cnf 729 {len(cnf[0])}\n")
+        for clause in cnf[0]:
+            write_cnf(f, clause, " 0\n")
+
+
 # dot denotes an empty cell
 sudokus = [
     [
@@ -41,7 +66,7 @@ s = Solver()
 
 variables = [
     [
-        [Bool(f"x_{i}_{j}_{c}") for c in range(1,10)] for j in range(9)
+        [Bool(f"x_{i}_{j}_{c}") for c in range(1, 10)] for j in range(9)
     ] for i in range(9)
 ]
 
@@ -60,14 +85,14 @@ for i in range(9):
 # every number only once per column
 for i in range(9):
     for j in range(8):
-        for k in range(j+1,9):
+        for k in range(j+1, 9):
             for c in range(9):
                 s.add(Not(And(variables[i][j][c], variables[i][k][c])))
 
 # every number only once per row
 for j in range(9):
     for i in range(8):
-        for k in range(i+1,9):
+        for k in range(i+1, 9):
             for c in range(9):
                 s.add(Not(And(variables[i][j][c], variables[k][j][c])))
 
@@ -79,7 +104,8 @@ for i in range(9):
             for i_prime in range(9):
                 for j_prime in range(9):
                     if (i // 3 == i_prime // 3 and j // 3 == j_prime // 3 and (i, j) != (i_prime, j_prime)):
-                        s.add(Implies(variables[i][j][c], Not(variables[i_prime][j_prime][c])))
+                        s.add(Implies(variables[i][j][c], Not(
+                            variables[i_prime][j_prime][c])))
 
 sudoku = sudokus[0]
 # add given numbers
@@ -88,17 +114,10 @@ for i in range(9):
         if sudoku[i][j] != '.':
             s.add(variables[i][j][int(sudoku[i][j]) - 1])
 
-# solve and print if satisfiable
-result = s.check()
-if result == sat:
-    m = s.model()
-    for i in range(9):
-        for j in range(9):
-            for k in range(9):
-                if m.evaluate(variables[i][j][k]):
-                    print(f"{k+1} ", end="")
-                    break
-        print()
-else:
-    print("unsatisfiable")
- 
+# convert to cnf
+t = Tactic('tseitin-cnf')
+goal = Goal()
+goal.add(s.assertions())
+cnf = t(goal)
+
+cnf_to_file("sudoku.cnf")
